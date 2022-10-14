7b9f30aceaed3844d2607b4f8d7644508826e377
petricaMilea <petrica.v.milea@gmail.com>
2022-10-14 18:32:24 +0300

    ex.17 - [Display(Name = "Book Title")] [Column(TypeName = "decimal(6, 2)")]

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,10 +1,17 @@
-﻿namespace Milea_Petrica_Vasile_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Milea_Petrica_Vasile_Lab2.Models
 {
     public class Book
     {
         public int ID { get; set; }
+
+        [Display(Name = "Book Title")]
         public string Title { get; set; }
         public string Author { get; set; }
+
+        [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
     }
 }
