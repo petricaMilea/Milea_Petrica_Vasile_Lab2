fb944f35a4fd882cd73be5f135e9714a89970421
petricaMilea <petrica.v.milea@gmail.com>
2022-10-28 21:40:06 +0300

    38-40 Category and BookCategory classes

---
+++ Models/BookCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace Milea_Petrica_Vasile_Lab2.Models
+{
+    public class BookCategory
+    {
+        public int Id { get; set; }
+        public int BookID { get; set; }
+        public Book Book { get; set; }
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Milea_Petrica_Vasile_Lab2.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+
+        public ICollection<BookCategory>? BookCategories { get; set; }
+
+    }
+}
