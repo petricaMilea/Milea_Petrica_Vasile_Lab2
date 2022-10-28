24ebc64a5bc5ea430d93e27c8b38ae1739cb7597
petricaMilea <petrica.v.milea@gmail.com>
2022-10-28 22:45:05 +0300

    stergere date din tabela books pentru a scapa de eroare la accesarea /Books

--- Migrations/20221028184903_BookCategory.Designer.cs
+++ Migrations/20221028184903_BookCategory.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Milea_Petrica_Vasile_Lab2.Data;
 
@@ -11,9 +12,10 @@ using Milea_Petrica_Vasile_Lab2.Data;
 namespace Milea_Petrica_Vasile_Lab2.Migrations
 {
     [DbContext(typeof(Milea_Petrica_Vasile_Lab2Context))]
-    partial class Milea_Petrica_Vasile_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20221028184903_BookCategory")]
+    partial class BookCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
