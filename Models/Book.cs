c068b2b725bcdf94eaafc664901c8b9dd249050c
petricaMilea <petrica.v.milea@gmail.com>
2022-11-05 10:02:55 +0200

    ex. 13-15

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,8 +8,17 @@ namespace Milea_Petrica_Vasile_Lab2.Models
     {
         public int ID { get; set; }
 
+        [Required, StringLength(150, MinimumLength = 3)]
         [Display(Name = "Book Title")]
         public string Title { get; set; }
+
+        [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$"), Required,
+StringLength(50, MinimumLength = 3)]
+        //^ marcheaza inceputul sirului de caractere
+        //[A-Z][a-z]+ prenumele -litera mare urmata de oricate litere mici
+        //\s spatiu
+        //[A-Z][a-z]+ numele- litera mare urmata de oricate litere mici
+        //$ marcheaza sfarsitul sirului de caractere
         public string Author { get; set; }
 
         [Column(TypeName = "decimal(6, 2)")]
