15661c0e7221d58fd4d1e2982de6168bb32253b9
petricaMilea <petrica.v.milea@gmail.com>
2022-12-10 08:15:02 +0200

    ex. 3 creare clasa

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,11 @@
+﻿using Milea_Petrica_Vasile_Lab2.Models;
+
+namespace Milea_Petrica_Vasile_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
